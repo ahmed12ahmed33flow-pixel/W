a561eab52d69a6f45c23845b4b33a0b7cfe8da51
ahmed12ahmed33flow-pixel <ahmed12ahmed33flow@gmail.com>
2026-07-18 16:14:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-alert("APP JS WORKING");
+const app = document.getElementById("app");
+
+app.innerHTML = `
+<h1>W</h1>
+
+<button onclick="hello()">
+اضغط هنا
+</button>
+`;
+
+function hello() {
+    alert("Welcome to W");
+}
