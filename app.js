b21ad961822b9ac1ff36bd4917f8d535440d1fbe
ahmed12ahmed33flow-pixel <ahmed12ahmed33flow@gmail.com>
2026-07-18 16:24:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-document.getElementById("app").innerHTML="<h1>W</h1>";
+document.getElementById("app").innerHTML =
+"<h1 style='color:red;font-size:70px;'>W</h1>" +
+"<h2>Welcome to W</h2>" +
+"<button id='btn'>تسجيل الدخول</button>";
+
+document.getElementById("btn").onclick = function () {
+    document.getElementById("app").innerHTML =
+    "<h1>الصفحة الرئيسية</h1>" +
+    "<p>Unlimited Movies & Series</p>";
+};
